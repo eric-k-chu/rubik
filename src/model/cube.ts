2dc5ca3ca4fed942ba3adedf76e4fd7f04655e64
eric-k-chu <kiyuen88ec@gmail.com>
2024-06-09 23:29:02 -0700

    wip piece

--- src/model/cube.ts
+++ src/model/cube.ts
@@ -1,6 +1,29 @@
-import { Orientation } from './notation'
+export type Position = {
+  x: number
+  y: number
+  z: number
+}
 
-export type Cube = {
-  orientation: Orientation
-  // color?? up to three colors, minimum one color
+export type CenterPiece = {
+  type: 'center'
+  color: string
+}
+
+export type EdgePiece = {
+  type: 'edge'
+  colorX: string
+  colorY: string
+}
+
+export type CornerPiece = {
+  type: 'corner'
+  colorX: string
+  colorY: string
+  colorZ: string
+}
+
+export type Piece = CenterPiece | EdgePiece | CornerPiece
+
+export type Cube = Piece & {
+  position: Position
 }
